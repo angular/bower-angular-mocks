139ec47a75c71fb19d7204a3a0b4d04414fce88e
jenkins <ci-bot@angularjs.org>
2014-01-27 23:51:04 -0800

    v1.2.11-build.2189+sha.95f0bf9

--- angular-mocks.js
+++ angular-mocks.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.11-build.2188+sha.8a0be35
+ * @license AngularJS v1.2.11-build.2189+sha.95f0bf9
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1954,7 +1954,7 @@ if(window.jasmine || window.mocha) {
 
   var currentSpec = null,
       isSpecRunning = function() {
-        return currentSpec && (window.mocha || currentSpec.queue.running);
+        return !!currentSpec;
       };
 
 
