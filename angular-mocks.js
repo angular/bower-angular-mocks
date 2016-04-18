f49860cbf88a448d9c43ce20a5f5a4ad7dc29d0e
jenkins <ci-bot@angularjs.org>
2016-04-18 04:48:55 -0700

    v1.5.6-build.4757+sha.c3de164

--- angular-mocks.js
+++ angular-mocks.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.5
+ * @license AngularJS v1.5.6-build.4757+sha.c3de164
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -452,7 +452,7 @@ angular.mock.$IntervalProvider = function() {
           promise = deferred.promise;
 
       count = (angular.isDefined(count)) ? count : 0;
-      promise.then(null, null, (!hasParams) ? fn : function() {
+      promise.then(null, function() {}, (!hasParams) ? fn : function() {
         fn.apply(null, args);
       });
 
@@ -512,6 +512,7 @@ angular.mock.$IntervalProvider = function() {
       });
 
       if (angular.isDefined(fnIndex)) {
+        repeatFns[fnIndex].deferred.promise.then(undefined, function() {});
         repeatFns[fnIndex].deferred.reject('canceled');
         repeatFns.splice(fnIndex, 1);
         return true;
